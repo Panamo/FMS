b52ee71892c5321599282e9c1255051561e0052c
Parham Alvani <parham.alvani@gmail.com>
2016-08-09 03:08:00 +0430

    Homepage was completed.

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,3 +1,9 @@
 @extends('layouts.app')
 @section('content')
+    <div class="page-header">
+        <h1>Welcome to FMS
+            <br>
+            <small>Fish Management System</small>
+        </h1>
+    </div>
 @endsection
