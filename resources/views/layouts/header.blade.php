fcd6d4dd5dc6612af5d990eef29308066653dcaa
Parham Alvani <parham.alvani@gmail.com>
2016-08-09 03:08:11 +0430

    Provide navigation bar :)

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -8,10 +8,30 @@
 <meta name="csrf-token" content="{{ csrf_token() }}">
 
 <title>
-    BMS @yield('title')
+    FMS @yield('title')
 </title>
 
 {!! Html::style('css/app.css') !!}
 
 </head>
 <body style="padding-bottom: 100px;">
+<nav class="navbar navbar-default">
+    <div class="container-fluid">
+        <div class="navbar-header">
+            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+                <span class="sr-only">Toggle navigation</span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+            </button>
+            <a class="navbar-brand" href="{{route('home')}}">FMS</a>
+        </div>
+
+        <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+            <ul class="nav navbar-nav">
+                <li><a href="{{route('vhip.create')}}">VHIP</a></li>
+            </ul>
+        </div>
+    </div>
+</nav>
+<div class="container">
