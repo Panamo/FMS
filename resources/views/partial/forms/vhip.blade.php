3dc8744f617a327fe8fe08a1d76ce7fa91e5fde4
Parham Alvani <parham.alvani@gmail.com>
2016-08-09 18:14:36 +0430

    VHIP form was completed.

--- resources/views/partial/forms/vhip.blade.php
+++ resources/views/partial/forms/vhip.blade.php
@@ -1,19 +1,34 @@
-<div>
-    <label for="letter_code">کد نامه</label><input type="text" id="letter_code" name="letter_code">
-    <label for="submit_date">تاریخ ثبت</label><input type="date" id="submit_date" name="submit_date">
-    <label for="expire_date">تاریخ انقضا</label><input type="date" id="expire_date" name="expire_date">
-</div>
-<hr>
-<div>
-    <label for="total_amount">مقدار کل</label> <input type="text" id="total_amount" name="total_amount">
-    <label for="tracking_code">کد رهگیری</label> <input type="text" id="tracking_code" name="tracking_code">
-</div>
-<hr>
-<div>
-    <label for="sub_companies">شرکت های زیرمجموعه</label>
-    <select name="sub_companies">
-        <option></option>
-    </select>
-    <label for="amount">مقدار</label> <input type="text" id="amount" name="amount"><button type="button" name="plus_button">+</button>
-</div>
-
+<form method="post" url="{{route('vhip.store')}}">
+    <div class="form-group">
+        <label for="letter_code">Letter Code</label>
+        <input class="form-control" type="text" id="letter_code" name="letter_code" placeholder="Letter Code">
+    </div>
+    <div class="form-group">
+        <label for="submit_date">Submit Date</label>
+        <input class="form-control" type="date" id="submit_date" name="submit_date">
+    </div>
+    <div class="form-group">
+        <label for="expire_date">Expire Date</label>
+        <input class="form-control" type="date" id="expire_date" name="expire_date">
+    </div>
+    <div class="form-group">
+        <label for="total_amount">Total Amount</label>
+        <input class="form-control" type="text" id="total_amount" name="total_amount" placeholder="Total Amount">
+    </div>
+    <div class="form-group">
+        <label for="tracking_code">Tracking Code</label>
+        <input class="form-control" type="text" id="tracking_code" name="tracking_code" placeholder="Tracking Code">
+    </div>
+    <hr>
+    <div class="form-group">
+        <label for="sub_companies">Sub Companies</label>
+        <select name="sub_companies" class="form-control">
+            <option></option>
+        </select>
+    </div>
+    <div class="form-group">
+        <label for="amount">Amount</label>
+        <input type="text" id="amount" name="amount">
+        <button type="button" name="plus_button">+</button>
+    </div>
+</form>
