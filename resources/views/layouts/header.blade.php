91657de5a3934f2f39d7eeae0a7a152ea021a570
Parham Alvani <parham.alvani@gmail.com>
2016-08-09 20:09:16 +0430

    Provide dropdown menu for VHIP.

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -29,7 +29,13 @@
 
         <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
             <ul class="nav navbar-nav">
-                <li><a href="{{route('vhip.create')}}">VHIP</a></li>
+                <li class="dropdown">
+                    <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">VHIP <span class="caret"></span></a>
+                    <ul class="dropdown-menu">
+                        <li><a href="{{route('vhip.index')}}">List</a></li>
+                        <li><a href="{{route('vhip.create')}}">New</a></li>
+                    </ul>
+                </li>
             </ul>
         </div>
     </div>
