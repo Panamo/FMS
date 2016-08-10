3decb85efb83e3e8d6fd0f9ee6969792f9a2d178
Parham Alvani <parham.alvani@gmail.com>
2016-08-10 05:56:57 +0430

    Company registration form was improved.

--- resources/views/partial/forms/company.blade.php
+++ resources/views/partial/forms/company.blade.php
@@ -6,8 +6,8 @@
     <div class="form-group">
       <label for="type">Type</label>
       <select name="type" id="type" class="form-control">
-          <option value="">ثبت</option>
-          <option value="">تکثیر</option>
+          <option value="register">Register</option>
+          <option value="clone">Clone</option>
       </select>
     </div>
     <div class="form-group">
