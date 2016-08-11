5622a1350a3fb9b59329fa248cc81f39873e0989
Parham Alvani <parham.alvani@gmail.com>
2016-08-11 06:38:26 +0430

    Company.js and vue plugin were added.

--- resources/assets/js/.eslintrc.js
+++ resources/assets/js/.eslintrc.js
@@ -2,6 +2,7 @@ module.exports = {
     "extends": "standard",
     "installedESLint": true,
     "plugins": [
-        "standard"
+        "standard",
+        "vue"
     ]
 };
---
+++ resources/assets/js/company.js
@@ -0,0 +1,22 @@
+/*
+ * +===============================================
+ * | Author:        Parham Alvani (parham.alvani@gmail.com)
+ * |
+ * | Creation Date: 11-08-2016
+ * |
+ * | File Name:     company.js
+ * +===============================================
+ */
+
+new Vue({
+  el: '#company-telephone-div',
+  data: {
+    n: 1
+  },
+  methods: {
+    addPhone: function (event) {
+      this.n++
+    }
+  }
+})
+
