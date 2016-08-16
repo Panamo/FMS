45aaf839a7c93b3cc1787a43adaed701ec2d4277
Parham Alvani <parham.alvani@gmail.com>
2016-08-16 23:57:25 +0430

    Just code reformatting :)

--- resources/views/company/index.blade.php
+++ resources/views/company/index.blade.php
@@ -1,26 +1,26 @@
 @extends('layouts.app')
 @section('content')
-    <table class="table table-striped table-hover">
-        <tr>
-            <th>Name</th>
-            <th></th>
-            <th></th>
-            <th></th>
-        </tr>
-        @forelse($companies as $company)
-        <tr>
-            <td>{{ $company->name }}</td>
-            <td><a class="btn btn-default" href="{{route('company.show', $company)}}">More Info</a></td>
-            <td><a class="btn btn-default" href="{{route('company.edit', $company)}}">Edit</a></td>
-            <td>
+<table class="table table-striped table-hover">
+    <tr>
+        <th>Name</th>
+        <th></th>
+        <th></th>
+        <th></th>
+    </tr>
+    @forelse($companies as $company)
+    <tr>
+        <td>{{ $company->name }}</td>
+        <td><a class="btn btn-default" href="{{route('company.show', $company)}}">More Info</a></td>
+        <td><a class="btn btn-default" href="{{route('company.edit', $company)}}">Edit</a></td>
+        <td>
             {{ Form::open(['url' => route('company.destroy', $company)]) }}
                 {{ Form::hidden('_method', 'DELETE') }}
                 <button type="submit" class="btn btn-warning">Delete</button>
             {{ Form::close() }}
-            </td>
-        </tr>
-        @empty
-            404 Not Found :D
-        @endforelse
-    </table>
+        </td>
+    </tr>
+    @empty
+    404 Not Found :D
+    @endforelse
+</table>
 @endsection
