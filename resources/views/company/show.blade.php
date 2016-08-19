c40f98f67b62b497c263297d19f63d5c0531597b
Parham Alvani <parham.alvani@gmail.com>
2016-08-19 18:12:45 +0430

    Just formatting improvement ...

--- resources/views/company/show.blade.php
+++ resources/views/company/show.blade.php
@@ -1,17 +1,17 @@
 @extends('layouts.app')
 @section('content')
-    <div class="panel panel-default">
-        <div class="panel-heading">
-            <h3 class="panel-title">{{ $company->name }}</h3>
-        </div>
-        <div class="panel-body">
-            {{ $company->address }}
-            <hr>
-            <ul>
-            @foreach($company->phones as $phone)
-                <li> {{ $phone }} </li>
-            @endforeach
-            </ul>
-        </div>
+<div class="panel panel-default">
+    <div class="panel-heading">
+        <h3 class="panel-title">{{ $company->name }}</h3>
     </div>
+    <div class="panel-body">
+        {{ $company->address }}
+        <hr>
+        <ul>
+            @foreach($company->phones as $phone)
+            <li> {{ $phone }} </li>
+            @endforeach
+        </ul>
+    </div>
+</div>
 @endsection
