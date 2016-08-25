758cfe74b1cc726b67e64b8097cdbeca9db16d6d
Parham Alvani <parham.alvani@gmail.com>
2016-08-25 08:51:00 +0430

    JQuery environment was added.

--- resources/assets/js/.eslintrc.js
+++ resources/assets/js/.eslintrc.js
@@ -1,6 +1,10 @@
 module.exports = {
     "extends": "standard",
     "installedESLint": true,
+    "env": {
+      "browser": true,
+      "jquery": true
+    },
     "plugins": [
         "standard",
         "vue"
